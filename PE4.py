c6e69ff601e22e1472b7f0720b8659eb90bb8d0d
Rayan Mostofa <r_mostofa@yahoo.com>
2020-12-24 20:20:12 -0700

    changed some stuff

--- PE4.py
+++ PE4.py
@@ -6,22 +6,22 @@ def palindrome(int_number):
     # even
     if length % 2 == 0:
         if str_number[:x] == str_number[:x-1:-1]:
-            return('True')
+            return True
         else:
-            return('False')
+            return False
 
     # odd
     else:
         if str_number[:x] == str_number[:x:-1]:
-            return('True')
+            return True
         else:
-            return('False')
+            return False
 
 greatest = 0
 
 for i in range(500, 1000):
     for j in range(500, 1000):
-        if palindrome(j*i) == 'True':
+        if palindrome(j*i):
             if (j*i) > greatest:
                 greatest = j*i
 
