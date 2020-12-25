24e1e83670aa04e2caf82710586b25b25ab0c4e4
Rayan Mostofa <r_mostofa@yahoo.com>
2020-12-25 16:00:46 -0700

    optimized PE10

--- PE10.py
+++ PE10.py
@@ -1,19 +1,36 @@
+from math import sqrt
+
 def prime(num):
-    if num == 1:
+    if num == 2:
+        return True
+    elif num % 2 == 0:
         return False
     else:
-        limit = round(num/2)
-        x = [i for i in range(1, limit + 1) if num % i == 0]
+        for i in range(2, round(sqrt(num)) + 1):
+            if num % i == 0:
+                return False
+        else:
+            return True
 
-        if 1 in x:
-            x.remove(1)
+def primes(num):
+    primes_list = [i for i in range(3, num + 1, 2)]
+    primes_list.insert(0, 2)
 
-        return True if x == [] else False
+    for index, value in enumerate(primes_list):
 
-prime_list = [2]
-difference = 0
+        if value == 0:
+            continue
 
-for i in range(1, 2000000, 2):
-    if prime(i):
-        prime_list.append(i)
-        print(f'Adding {i}')
+        elif not prime(value):
+            primes_list[index] = 0
+
+        elif prime(value):
+            for index_j, value_j in enumerate(primes_list[index:]):
+                    if value_j % value == 0:
+                        primes_list[index_j] = 0
+
+    primes_list = [i for i in primes_list if i != 0]
+
+    return primes_list
+
+print(sum(primes(2000000)))
